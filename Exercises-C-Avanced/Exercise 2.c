fc5dcb9acf95848995e80886a95aefa68a2c9701
ronius1 <101593555+ronius1@users.noreply.github.com>
2022-03-15 17:53:04 +0200

    check

--- Exercises-C-Avanced/Exercise 2.c
+++ Exercises-C-Avanced/Exercise 2.c
@@ -2,7 +2,7 @@
 //  main.c
 //  Exercises-C-Avanced
 //
-//  Created by Ron Arugeti 
+//  Created by Ron Arugeti
 //
 #include <string.h>
 #include <stdio.h>
@@ -27,6 +27,8 @@ struct WorkerList* next;
 Worker* CreateWorker(unsigned long id, char* name, unsigned long sel, char year1[7], unsigned long year2, int start);
 void PrintWorker(Worker* wr, int start);
 WorkerList * addWorker(WorkerList *head, Worker * w);
+int index_r(WorkerList* head, unsigned long id);
+int index_l(WorkerList* head, unsigned long id);
 
 int main() {
     printf("How are you!\n");
@@ -54,8 +56,13 @@ else
    static WorkerList *temp1, *temp2, *temp3;
     do {
         temp3 = head;
+        if (!head) {
+            temp1->data = w;
+            return temp1;
+        }
     } while (!temp2);
-    if(head->data->Selary > w->Selary){
+    
+    if(head->data->Selary > w->Selary || !head){
         temp1->data = w;
         temp1->next = head;
         temp2->next = temp1;
@@ -63,4 +70,26 @@ else
     }
     temp2 = head;
     return addWorker(head->next, w);
+}int index_r(WorkerList* head, unsigned long id){
+    static int k = 1;
+    if (head->data->ID == id){
+        return k;
+    }
+    k++;
+    index_r(head->next, id);
+    return -1;
+}int index_l(WorkerList* head, unsigned long id){
+    int k = -1;
+    WorkerList* temp;
+    temp = head;
+    while (temp) {
+        k++;
+        if (temp->data->ID == id) {
+            k++;
+        }
+        temp = temp->next;
+    }
+    return k;
 }
+
+
