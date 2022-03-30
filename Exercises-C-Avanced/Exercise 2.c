56923a64c2f775e60260e3d302df72a7c5fcf18d
ronius1 <101593555+ronius1@users.noreply.github.com>
2022-03-30 17:53:18 +0300

    עודכן

--- Exercises-C-Avanced/Exercise 2.c
+++ Exercises-C-Avanced/Exercise 2.c
@@ -1,10 +1,4 @@
-//
-//  main.c
-//  Exercises-C-Avanced
-//
-//  Created by Ron Arugeti
-//
-#define SECURE_NO_WORNINGS
+#define _CRT_SECURE_NO_WORNINGS
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,44 +27,41 @@ int index_l(WorkerList* head, unsigned long id);
 void update_worker(WorkerList *head, float percent);
 WorkerList *reverse(WorkerList *head);
 void freeWorkers(WorkerList *head);
+void print_e(void);
+
+unsigned long id = 0, salary = 0, g_year = 0;
+    char h_year[6];
+    char* full_name;
+    int start;
 
 int main() {
+    WorkerList List_w;
     Worker* worker1 = (struct Worker*)malloc(sizeof(struct Worker));
-    unsigned long id = 0, salary = 0, g_year = 0;
-    char h_year[6];
-    char* full_name = (char*)malloc(sizeof(SIZE));
-    int start;
-    WorkerList* List_w = NULL;
-    
-    if (!worker1 || !full_name) {
-        printf("allocation failed!\n");
-        exit(1);
-    }
-    
-    printf("Please enter your ID: ");
-    scanf("%lu", &id);
-    printf("Please enter full name: ");
-    fseek(stdin, 0, SEEK_END);
-    fgets(full_name, SIZE, stdin);
-    *(full_name + (int)strlen(full_name) -1) = '\0';
-    printf("Please enter your salary:  ");
-    scanf("%lu", &salary);
-    printf("Please enter 1 if you used to write years in hebrew calndery or 0 if you used to write years in gregorian calendry: ");
-    scanf("%d",&start);
-    printf("Please enter the year you have started to work:  \n");
-    fseek(stdin, 0, SEEK_END);
-    if (start)
-        scanf("%s",h_year);
-    else
-        scanf("%lu",&g_year);
+    Worker* worker2 = (struct Worker*)malloc(sizeof(struct Worker));
+    Worker* worker3 = (struct Worker*)malloc(sizeof(struct Worker));
+    if (!worker1 || !worker2 || !worker3){ printf("allocation failed!\n"); exit(1);}
+    print_e();
     worker1 = CreateWorker(id, full_name, salary, h_year, g_year, start);
     free(full_name);
-    PrintWorker(worker1, 1);
-    addWorker(List_w, worker1);
+    print_e();
+    worker2 = CreateWorker(id, full_name, salary, h_year, g_year, start);
+    free(full_name);
+    print_e();
+    worker3 = CreateWorker(id, full_name, salary, h_year, g_year, start);
+    free(full_name);
 
-    
+    addWorker(&List_w, worker1);
+    addWorker(&List_w, worker2);
+    addWorker(&List_w, worker3);
+
+    PrintWorker((&List_w)->data, 1);
+    PrintWorker((&List_w)->next->data, 1);
+    PrintWorker((&List_w)->next->next->data, 1);
+
     return 0;
-}Worker* CreateWorker(unsigned long id, char* name, unsigned long sal, char year1[7], unsigned long year2, int start){
+}
+Worker* CreateWorker(unsigned long id, char* name, unsigned long sal, char year1[7], unsigned long year2, int start)
+{
     fseek(stdin, 0, SEEK_END);
     Worker* Wr = (Worker*)malloc(sizeof(Worker)); //memory allocation for "Wr" - which is pointer for worker structure
     if (!Wr) {
@@ -90,7 +81,9 @@ int main() {
     else
         Wr->year.global = year2;
     return Wr;
-}void PrintWorker(Worker* wr,int start){
+}
+void PrintWorker(Worker* wr,int start)
+{
     printf("Worker's Name: %sֿֿ\n", wr->Name);
     printf("Worker's ID: %lu\n", wr->ID);
     printf("Worker's Salery: %lu\n", wr->Salary);
@@ -98,7 +91,9 @@ if (start)
     printf("Worker's starting year:%s\n", wr->year.hebrew);
 else
     printf("Worker's starting year: %lu\n", wr->year.global);
-}WorkerList *addWorker(WorkerList *head, Worker *w){
+}
+WorkerList *addWorker(WorkerList *head, Worker *w)
+{
     WorkerList *temp = (struct WorkerList*)malloc(sizeof(struct WorkerList));
     if (!temp) {printf("allocation failed");exit(1);}
     if (!head){
@@ -112,7 +107,9 @@ else
     }
     temp->next->data = w;
     return temp;
-}int index_r(WorkerList* head, unsigned long id){
+}
+int index_r(WorkerList* head, unsigned long id)
+{
     static int k = 1;
     if (head->data->ID == id){
         return k;
@@ -120,7 +117,9 @@ else
     k++;
     index_r(head->next, id);
     return -1;
-}int index_l(WorkerList* head, unsigned long id){
+}
+int index_l(WorkerList* head, unsigned long id)
+{
     int k = 1;
     WorkerList* temp;
     temp = head;
@@ -134,7 +133,9 @@ else
         k++;
     }
     return k;
-}WorkerList* deleteWorstWorker(WorkerList *head){
+}
+WorkerList* deleteWorstWorker(WorkerList *head)
+{
  static WorkerList *temp1 = NULL, *temp2 = NULL;
     if (!temp2){
         temp1 = head;
@@ -158,13 +159,18 @@ else
     if (head->data->Salary < temp2->data->Salary)
     temp2 = head;
     return deleteWorstWorker(head->next);
-}void update_worker(WorkerList *head, float percent){
+}
+void update_worker(WorkerList *head, float percent)
+{
     WorkerList* temp = head;
     while (temp) {
         temp->data->Salary = (unsigned long)(temp->data->Salary * (1+ percent));
         temp->next = temp;
     }
-}WorkerList *reverse(WorkerList *head){
+    return;
+}
+WorkerList *reverse(WorkerList *head)
+{
    static WorkerList *temp;
     if (!head->next) {
         return head;
@@ -173,11 +179,35 @@ else
     temp->next = head;
     head->next = NULL;
     return temp;
-}void freeWorkers(WorkerList *head){
+}
+void freeWorkers(WorkerList *head)
+{
     static WorkerList *temp;
     while (!head) {
         temp = head;
         head = head->next;
         free(temp->data->Name);
         free(temp->data);
-    }}
+    }
+    return;
+}
+void print_e(void)
+{
+    full_name = (char*)malloc(sizeof(SIZE));
+    printf("Please enter your ID: ");
+    scanf("%lu", &id);
+    printf("Please enter full name: ");
+    fseek(stdin, 0, SEEK_END);
+    fgets(full_name, SIZE, stdin);
+    *(full_name + (int)strlen(full_name) -1) = '\0';
+    printf("Please enter your salary:  ");
+    scanf("%lu", &salary);
+    printf("Please enter 1 if you used to write years in hebrew calndery or 0 if you used to write years in gregorian calendry: ");
+    scanf("%d",&start);
+    printf("Please enter the year you have started to work:  \n");
+    fseek(stdin, 0, SEEK_END);
+    if (start)
+        scanf("%s",h_year);
+    else
+        scanf("%lu",&g_year);
+    }
